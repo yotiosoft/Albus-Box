d51e992e495fcfd1a9979c63ab70584f0959bff5
DESKTOP-OJNV5ND\ytani <ytani0323@gmail.com>
2021-08-06 21:51:03 +0900

    AlbusBox.cppを作成

--- src/AlbusBox.hpp
+++ src/AlbusBox.hpp
@@ -1,13 +1,12 @@
-/*
 #pragma once
 
 #ifndef AlbusBox_hpp
 #define AlbusBox_hpp
 
-#include "ButtonEx.hpp"
-#include "Enums.hpp"
 #include <Specific.hpp>
+#include <NeumorphismUI.hpp>
 
+pair<bool, FilePath> FileOpen();
 void AlbusBox();
 
 #endif /* AlbusBox_hpp */
