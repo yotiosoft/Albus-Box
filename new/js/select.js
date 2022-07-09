d6f633a9c75dbc5c2b52cea798cfdc394d03f843
YotioSoft <ytani0323@gmail.com>
2022-07-10 02:21:37 +0900

    fix os checker

--- new/js/select.js
+++ new/js/select.js
@@ -1,22 +1,14 @@
-if (navigator.userAgent.indexOf("Win") != -1) {
-    switch_to_windows();
-}
-else if (navigator.userAgent.indexOf("Mac") != -1) {
-    switch_to_mac();
-}
-else if (navigator.userAgent.indexOf("Linux") != -1) {
-    switch_to_linux();
-}
-
 function switch_to_windows() {
     let current_option = document.getElementsByClassName("os_select is_active")[0];
-    current_option.classList.remove("is_active");
+    if (current_option != null)
+        current_option.classList.remove("is_active");
 
     let selected_option = document.getElementById("select_windows");
     selected_option.classList.add("is_active");
 
     let current_view = document.getElementsByClassName("os_document is_active")[0];
-    current_view.classList.remove("is_active");
+    if (current_view != null)
+        current_view.classList.remove("is_active");
 
     let selected_view = document.getElementById("switch_view_windows");
     selected_view.classList.add("is_active");
@@ -38,13 +30,15 @@ function switch_to_windows() {
 
 function switch_to_macos() {
     let current_option = document.getElementsByClassName("os_select is_active")[0];
-    current_option.classList.remove("is_active");
+    if (current_option != null)
+        current_option.classList.remove("is_active");
 
     let selected_option = document.getElementById("select_macos");
     selected_option.classList.add("is_active");
 
     let current_view = document.getElementsByClassName("os_document is_active")[0];
-    current_view.classList.remove("is_active");
+    if (current_view != null)
+        current_view.classList.remove("is_active");
 
     let selected_view = document.getElementById("switch_view_macos");
     selected_view.classList.add("is_active");
@@ -66,13 +60,15 @@ function switch_to_macos() {
 
 function switch_to_linux() {
     let current_option = document.getElementsByClassName("os_select is_active")[0];
-    current_option.classList.remove("is_active");
+    if (current_option != null)
+        current_option.classList.remove("is_active");
 
     let selected_option = document.getElementById("select_linux");
     selected_option.classList.add("is_active");
 
     let current_view = document.getElementsByClassName("os_document is_active")[0];
-    current_view.classList.remove("is_active");
+    if (current_view != null)
+        current_view.classList.remove("is_active");
 
     let selected_view = document.getElementById("switch_view_linux");
     selected_view.classList.add("is_active");
@@ -90,5 +86,20 @@ function switch_to_linux() {
         for (var selected_img of selected_imgs) {
             selected_img.classList.add("is_active");
         }
+    }
+}
+
+window.onload = function() {
+    if (navigator.userAgent.indexOf("Win") != -1) {
+        switch_to_windows();
+    }
+    else if (navigator.userAgent.indexOf("Mac") != -1) {
+        switch_to_macos();
+    }
+    else if (navigator.userAgent.indexOf("Linux") != -1) {
+        switch_to_linux();
+    }
+    else {
+        switch_to_windows();
     }
 }
