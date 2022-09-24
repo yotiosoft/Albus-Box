8e17a67d01a914862476a6e12e74cfb0d723006c
YotioSoft <ytani0323@gmail.com>
2022-09-24 23:36:34 +0900

    remove unused functions in OtherFunctions

--- src/OtherFunctions.cpp
+++ src/OtherFunctions.cpp
@@ -7,54 +7,6 @@
 
 #include "OtherFunctions.hpp"
 
-Array<string> splitUTF8(string str, string separator) {
-	auto separatorLength = separator.length();
-	
-	Array<string> list = Array<string>();
-	
-	if (separatorLength == 0) {
-		list.push_back(str);
-	} else {
-		auto offset = std::string::size_type(0);
-		while (1) {
-			auto pos = str.find(separator, offset);
-			if (pos == std::string::npos) {
-				list.push_back(str.substr(offset));
-				break;
-			}
-			list.push_back(str.substr(offset, pos - offset));
-			offset = pos + separatorLength;
-		}
-	}
-	
-	return list;
-}
-Array<String> split(String str, String separator) {
-	string strUTF8 = str.toUTF8();
-	string separatorUTF8 = separator.toUTF8();
-	
-	auto separatorLength = separator.length();
-	
-	Array<String> list = Array<String>();
-	
-	if (separatorLength == 0) {
-		list.push_back(str);
-	} else {
-		auto offset = std::string::size_type(0);
-		while (1) {
-			auto pos = strUTF8.find(separatorUTF8, offset);
-			if (pos == std::string::npos) {
-				list.push_back(Unicode::Widen(strUTF8.substr(offset)));
-				break;
-			}
-			list.push_back(Unicode::Widen(strUTF8.substr(offset, pos - offset)));
-			offset = pos + separatorLength;
-		}
-	}
-	
-	return list;
-}
-
 void addTextFile(string filePath, string str) {
 	ofstream ofs;
 	ofs.open(filePath, ios::out | ios::binary | ios::app);
@@ -64,15 +16,4 @@ void addTextFile(string filePath, string str) {
 
 void debugLog(String str) {
 	addTextFile("log.txt", str.toUTF8()+"\n");;
-}
-
-string replaceString(string beforeStr, string searchStr, string afterStr) {
-	string::size_type pos(beforeStr.find(searchStr));
-	
-	while (pos != string::npos) {
-		beforeStr.replace(pos, searchStr.length(), afterStr);
-		pos = beforeStr.find(searchStr, pos + afterStr.length());
-	}
-	
-	return beforeStr;
 }
